e5f4eafc75a5810b7deeef3583ff91f928a2eabd
natwhiskey <kimngza@hotmail.com>
2021-08-27 03:26:30 +0700

    create function searchphotos build if and for loop

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+function searchPhotos(event) {
+    const keyword = event.target.value;
+    if (event.key === 'Enter' && keyword) {
+        removePhotos();
+        for (let i = 0; i < 10; i++) {
+            appendImageElement(keyword, i);
+        }
+    }
+}
 function run() {
     const inputElement = document.querySelector('input');
     inputElement.addEventListener('keydown', searchPhotos);
